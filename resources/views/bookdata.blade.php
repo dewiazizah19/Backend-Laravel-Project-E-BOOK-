d43e80ca122326631f13f98c8e8dad1140a33e4a
Dewi Isarotul Azizah <dewiazizah968@gmail.com>
2021-10-13 19:01:07 +0700

    second commit

--- resources/views/bookdata.blade.php
+++ resources/views/bookdata.blade.php
@@ -5,7 +5,7 @@
 <section class="content-header">
     <h1>
       Data Buku
-      <small>advanced tables</small>
+      <small>Data Genre Buku</small>
     </h1>
     <ol class="breadcrumb">
       <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
@@ -24,11 +24,18 @@
               <section id="new">
                 <h4 class="page-header">Non Fiksi</h4>
                 <div class="row fontawesome-icon-list">
-                  <div class="col-md-3 col-sm-4"><i class="fa fa-fw fa-map"></i>Ensiklopedia</div>
-                  <div class="col-md-3 col-sm-4"><i class="fa fa-fw fa-clone"></i>Biografi</div>
-                  <div class="col-md-3 col-sm-4"><i class="fa fa-fw fa-calendar-minus-o"></i>Sejarah</div>
-                  <div class="col-md-3 col-sm-4"><i class="fa fa-fw fa-object-ungroup"></i>Sains Populer</div>
-                  <div class="col-md-3 col-sm-4"><i class="fa fa-fw fa-hourglass"></i>Filsafat</div>
+                  <div class="col-md-3 col-sm-4"><a href="./transaksi.html"><img src="Template/dist/img/ensiklopedia.png" class="user-image" alt="User Image"></a>Ensiklopedia</div>
+                  <div class="col-md-3 col-sm-4"><img src="Template/dist/img/biografi.png" class="user-image" alt="User Image">Biografi</div>
+                  <div class="col-md-3 col-sm-4"><img src="Template/dist/img/sejarah.png" class="user-image" alt="User Image">Sejarah</div>
+                  <div class="col-md-3 col-sm-4"><img src="Template/dist/img/sains.png" class="user-image" alt="User Image">Sains Populer</div>
+                  <div class="col-md-3 col-sm-4"><img src="Template/dist/img/filsafat.png" class="user-image" alt="User Image">Filsafat</div>
+                  <div class="col-md-3 col-sm-4"><img src="Template/dist/img/autobiografi.png" class="user-image" alt="User Image">Autobiografi</div>
+                  <div class="col-md-3 col-sm-4"><img src="Template/dist/img/memoir.png" class="user-image" alt="User Image">Memoir</div>
+                  <div class="col-md-3 col-sm-4"><img src="Template/dist/img/kamus.png" class="user-image" alt="User Image">Kamus</div>
+                  <div class="col-md-3 col-sm-4"><img src="Template/dist/img/jurnalis.png" class="user-image" alt="User Image">Jurnal</div>
+                  <div class="col-md-3 col-sm-4"><img src="Template/dist/img/psikologi.png" class="user-image" alt="User Image">Psikologi</div>
+                  <div class="col-md-3 col-sm-4"><img src="Template/dist/img/travel.png" class="user-image" alt="User Image">Catatan Travel</div>
+                  <div class="col-md-3 col-sm-4"><img src="Template/dist/img/humor.png" class="user-image" alt="User Image">Humor</div>
                 </div>
               </section>
           </div>
